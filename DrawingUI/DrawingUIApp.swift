5b6e97e7e12d7f7b137ad97678330fcd35117b4b
Maxim Vainikka <rithmique@yahoo.com>
2021-05-09 08:47:30 +0200

    Initial Commit

---
+++ DrawingUI/DrawingUIApp.swift
@@ -0,0 +1,17 @@
+//
+//  DrawingUIApp.swift
+//  DrawingUI
+//
+//  Created by Максим Нуждин on 09.05.2021.
+//
+
+import SwiftUI
+
+@main
+struct DrawingUIApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
